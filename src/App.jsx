3de92086f370cf015a90afb49e45e3bfaab1b469
kojomens566-create <kojomens566@gmail.com>
2026-03-20 10:36:19 +0000

    Create src/App.jsx file with StudentVote React component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StudentVote = () => {
+    return (
+        <div>
+            <h1>Welcome to Student Vote App</h1>
+        </div>
+    );
+};
+
+export default StudentVote;
